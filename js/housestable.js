cfaba4a5780198dc59f4e26d4954d6dd2ee9df9a
Jérémy Libion <jlibion@zoho.com>
2024-03-19 20:41:52 -0400

    First version

---
+++ js/housestable.js
@@ -0,0 +1,89 @@
+let table = document.getElementById("houses");
+let fragment = document.createDocumentFragment();
+let thead = document.createElement('thead');
+let tbody = document.createElement('tbody');
+
+const currency = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'CAD',
+    maximumFractionDigits: 0,
+  });
+
+(async() => {
+    /* parse workbook */
+    const url = "houses.xlsx";
+    const workbook = XLSX.read(await (await fetch(url)).arrayBuffer());
+  
+    /* get first worksheet */
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const raw_data = XLSX.utils.sheet_to_json(worksheet, {header:1});
+
+    raw_data.forEach((row, index) => {
+        let tr = document.createElement('tr');
+        for (let i = 0; i < row.length; i++) {
+            let cell;
+            if (index === 0) {
+                cell = document.createElement('th');
+            } else {
+                cell = document.createElement('td');
+            }
+            if (row[i] == undefined) {
+                cell.innerText = "";
+            } else if (i === 0 && index !== 0) {
+                cell.innerText = currency.format(row[i]);
+            } else if (i === 3 && index !== 0) {
+                let arr = row[i].split('\n');
+                arr.forEach(el => {
+                    let span = document.createElement('span');
+                    span.classList.add('badge','text-bg-secondary');
+                    span.innerText = el;
+                    cell.appendChild(span);
+                })
+            } else if (i === row.length-1 && index !== 0) {
+                let btn = document.createElement('a');
+                btn.setAttribute('type',"button");
+                btn.classList.add('link-primary');
+                btn.setAttribute('data-bs-toggle',"modal");
+                btn.setAttribute('data-bs-target',"#frame");
+                btn.setAttribute('data-jl-src',row[i]);
+                btn.setAttribute('data-jl-loc',row[1])
+                btn.innerText = row[i];
+                cell.appendChild(btn);
+            } else {
+                cell.innerText = row[i];
+            }
+            tr.appendChild(cell);
+        }
+        if (index === 0) {
+            thead.appendChild(tr);
+        } else {
+            tbody.appendChild(tr);
+        }
+    });
+    fragment.appendChild(thead);
+    fragment.appendChild(tbody);
+    table.appendChild(fragment);
+})();
+
+const modal = document.getElementById('frame')
+if (modal) {
+  modal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const loc = button.getAttribute('data-jl-loc');
+    console.log(loc);
+    const src = button.getAttribute('data-jl-src');
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = modal.querySelector('.modal-title');
+    const modalNewTab = modal.querySelector('#newtab');
+    const modalIFrame = modal.querySelector('.modal-body iframe');
+
+    modalTitle.textContent = loc;
+    modalNewTab.setAttribute('href',src);
+    modalIFrame.setAttribute('src',src);
+  })
+};
